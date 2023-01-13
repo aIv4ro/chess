7f55ae6a351ddc73e066a46aa6b1a7dc985b1cd7
Alvaro <alvaromagu2002@gmail.com>
2023-01-13 18:10:37 +0100

    initial commit

--- src/app/components/game-board/game-board.component.spec.ts
+++ src/app/components/game-board/game-board.component.spec.ts
@@ -1,23 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { GameBoardComponent } from './game-board.component';
 
 describe('GameBoardComponent', () => {
-  let component: GameBoardComponent;
-  let fixture: ComponentFixture<GameBoardComponent>;
+	let component: GameBoardComponent;
+	let fixture: ComponentFixture<GameBoardComponent>;
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [GameBoardComponent]
+		}).compileComponents();
+		fixture = TestBed.createComponent(GameBoardComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ GameBoardComponent ]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(GameBoardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
--- src/app/components/game-board/game-board.component.ts
+++ src/app/components/game-board/game-board.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-game-board',
-  templateUrl: './game-board.component.html',
-  styleUrls: ['./game-board.component.scss']
+	selector: 'app-game-board',
+	templateUrl: './game-board.component.html',
+	styleUrls: ['./game-board.component.scss']
 })
 export class GameBoardComponent {
 
