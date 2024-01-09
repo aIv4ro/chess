372971bef300dec0569eab925e9af67786bd6171
Alvaro <alvaromagu2002@gmail.com>
2024-01-09 20:49:29 +0100

    fix format

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -18,7 +18,8 @@ export function Board () {
         <button
           className='px-3 py-2 rounded bg-slate-800 hover:bg-slate-600 transition-colors'
           onClick={handleCopyFen}
-        >Copy FEN
+        >
+          Copy FEN
         </button>
       </header>
       <div className='flex justify-center'>
